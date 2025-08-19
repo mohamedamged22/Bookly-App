27839a9be795e145c643d1839121d49a71c76938
mohamedamged22 <mohamedamgedzena@gmail.com>
2025-08-19 12:13:27 +0300

    create title and style it

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/core/utils/style.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,12 +8,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(),
-         FeaturedListView(),
-      
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomAppBar(),
+          FeaturedListView(),
+          SizedBox(height: 50),
+          Text('Best Seller', style: Style.titleMedium),
+        ],
+      ),
     );
   }
 }
