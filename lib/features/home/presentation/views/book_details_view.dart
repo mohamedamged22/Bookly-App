041368122105529d739d4d18731e42554055d11f
mohamedamged22 <mohamedamgedzena@gmail.com>
2025-08-19 17:42:39 +0300

    add router to navigte

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,14 @@
+import 'package:booklyapp/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
