a3fa18a10d45bc89dc8ce769b73c707511da4bb5
mohamedamged22 <mohamedamgedzena@gmail.com>
2025-08-23 15:45:55 +0300

    create newest books cubit

---
+++ lib/features/home/presentation/manger/newest_book_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:booklyapp/features/home/data/book_model/book_model.dart';
+import 'package:booklyapp/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+    final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failur) => emit(NewestBooksFailure(failur.errorMessage)),
+      (success) => emit(NewestBooksSuccess(success)),
+    );
+  }
+}
